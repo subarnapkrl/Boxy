3bf2ca42e8ceb3a79c92d89908b729f093bd2137
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-11-13 05:38:56 +0545

    SceneManagement-Checked

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
@@ -22,6 +23,19 @@ public class Score : MonoBehaviour
             Destroy(other.gameObject);
             scoreText.text="COINS: "+ scoreNum;
         }
+        
+    }
+     private void Update() {
+        if(scoreNum==5)
+        {
+            Invoke("CompleteLevel",2f);
+            
+        }   
+    }
+
+    void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
     
 }
