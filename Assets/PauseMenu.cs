602544b6545f936056dcb106db2c8f7cd9b17e16
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-11-15 05:30:08 +0545

    Pause-Menu-Completed

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -3,49 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class PauseMenu : MonoBehaviour
 {
-    public static bool gameIsPaused=false;
+    [SerializeField] GameObject pauseMenu;
 
-    public GameObject pauseMenuUI;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(gameIsPaused)
-            {
-                Resume();
-            }else{
-                Paused();
-            }
-            
-        }
-       
+    public void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale=0f;
     }
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
+        pauseMenu.SetActive(false);
         Time.timeScale=1f;
-        gameIsPaused=false;
     }
-    public void Paused()
+    public void Home(int sceneId)
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale=0f;
-        gameIsPaused=true;
+        Time.timeScale=1f;
+        SceneManager.LoadScene(sceneId);
     }
-    public void Menuuu()
-    {
-        SceneManager.LoadScene("Menu");
-    }
-
+   
 
 }
